a67f70847ae3b99552d743c7b1782f96b05fa549
Rasuole7 <widercreativeperspectives@gmail.com>
2023-05-13 19:53:14 +0300

    Zuza testai

--- src/main/java/Zuza/ZuzaTest.java
+++ src/main/java/Zuza/ZuzaTest.java
@@ -7,66 +7,60 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.awt.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ZuzaTest extends ZuzaDraiveriai {
 
-    public ZuzaTest() {
+            public ZuzaTest() {
 
-        super(driver);
-    }
+            super(driver);
+        }
 
     @BeforeClass
     public static void setUp() {
-        System.setProperty("Webdriver.chrome.driver", "/Users/Raimonda/Downloads/chromedriver/chromedriver");
-//       System.setProperty("webdriver.chrome.driver", "C:/Users/XPS 15/Downloads/chromedriver_win32/chromedriver.exe");
+//       System.setProperty("Webdriver.chrome.driver", "/Users/Raimonda/Downloads/chromedriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/XPS 15/Downloads/chromedriver_win32 (1)/" +
+                "chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         ZuzaDraiveriai.driver = new ChromeDriver(options);
         ZuzaDraiveriai.driver.manage().window().maximize();
     }
 
-//       System.setProperty("Webdriver.chrome.driver", "/Users/Raimonda/Downloads/chromedriver/chromedriver");
-//    //       System.setProperty("webdriver.chrome.driver", "C:/Users/XPS 15/Downloads/chromedriver_win32/chromedriver.exe");
-//    ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-notifications");
-//    ZuzaDraiveriai.driver = new FirefoxDriver(options);
-//        ZuzaDraiveriai.driver.manage().window().maximize();
-//        }
-
-    //    @Test
+//    @Test
 //    public void A_registracijosPasiekimas(){
 //            ZuzaRegistracija.slapukas();
 //            ZuzaRegistracija.prisiregistruoti("Nakciausias5098@gmail.com", "Grazuma8597");
 //    }
 //
 //    @Test
-//    public void B_blogaRegistracija() {
+//    public void B_blogaRegistracija(){
 //        ZuzaRegistracija.prisiregistruoti("Geguteskukuoja@@gmail.com", "kukukukuku");
 //
-//    }
+
 //
 //    @Test
 //    public void C_blogasPrisijungimas(){
 //        ZuzaDraiveriai.zuzaPuslapis();
-//        ZuzaPrisijungimas.prisijungimas("pumabera@gmail.com", "baigiamasis");
+//        ZuzaPrisijungimas.prisijungimas("pumabera@gmaiil.com", "baigiamasis");
 //    }
 //
-//    @Test
-//    public void D_prisijungimas() {
-//        ZuzaDraiveriai.zuzaPuslapis();
-//        ZuzaPrisijungimas.prisijungimas("Nakciausias50987@gmail.com", "Grazuma8597");
-//    }
+    @Test
+    public void D_prisijungimas(){
+        ZuzaDraiveriai.zuzaPuslapis();
+        ZuzaPrisijungimas.prisijungimas("Nakciausias50987@gmail.com", "Grazuma8597");
+    }
 
 
 //    @Test
-//    public void E_paieska(){
+//    public void F_paieska(){
 //        PaieskosLaukas.zuzaPaieska();
 //    }
 //
 //    @Test
-//    public void F_pirkiniai(){
+//    public void E_pirkiniai(){
 //        PirkiniuKrepselis.zuzaPirkiniai();
 //    }
 
@@ -75,30 +69,42 @@ public class ZuzaTest extends ZuzaDraiveriai {
 //            ZuzaRegistracija.paskyrosNaikinimas();
 //    }
 
-    //   @Test
+//   @Test
 //   public void H_megstamiausiuPirkiniuKrepselis() throws InterruptedException {
 //            ZuzaMegstamiausiuSarasas.MegstamiausiuSarasas();
+
 //   }
-//
+
 //   @Test
-//   public void H_naikinamasMegstamiausiuPirkiniuSarasas() throws InterruptedException {
+//   public void I_naikinamasMegstamiausiuPirkiniuSarasas() throws InterruptedException {
 //       ZuzaMegstamiausiuSarasas.MegstamiausiuSarasas();
 //       ZuzaMegstamiausiuSarasas.MegstamiausiuSarasoNaikinimas();
 //   }
+
 //    @Test
-//    public void I_naujoAdresoIdejimas(){
-//                ZuzaAdresai.pridetiNaujaAdresaF();
+//    public void J_redagavimasProfilio() throws InterruptedException {
+//        ZuzaPrisijungimas.prisijungimas("Nakciausias50987@gmail.com", "Murmaliai853");
+//        ZuzaProfilioRedagavimas.profilioRedagavimas();
+//        ZuzaProfilioRedagavimas.slaptazodzioKeitimas("Murmaliai853", "Grazuma8597");
 //    }
+
+//    @Test
+//    public void K_prekesGrazinimas() throws InterruptedException {
+//        ZuzaPrekesGrazinimas.prekesGrazinimas();
+//        ZuzaPrekesGrazinimas.puslapioPaveikslelis();
+//    }
+
     @Test
-    public void J_meniu() {
-//        ZuzaMeniu.verifyNavigationLink(driver, "Apmokėjimas", "https://zuza.lt/pages/apmokejimas/");
-        ZuzaMeniu.testuoju();
+    public void I_klausimasDelNepateiktoUzsakymo() throws InterruptedException, AWTException {
+
+                ZuzaNepateiktasUzsakymas.klausimasDelNepateiktoUzsakymo();
     }
+
 
 
     @AfterClass
     public static void tearDown() {
-            driver.quit();
+//            driver.quit();
     }
 }
 
