6eeb2e9a635f947fdde163e46f720b0083b06d73
Rasuole7 <widercreativeperspectives@gmail.com>
2023-05-13 19:52:56 +0300

    Zuza profilio redagavimas: lytis, gimimo data, vardas, pavardė, slaptažodžio keitimas

--- src/main/java/Zuza/ZuzaProfilioRedagavimas.java
+++ src/main/java/Zuza/ZuzaProfilioRedagavimas.java
@@ -12,8 +12,9 @@ import static org.junit.Assert.assertEquals;
 public class ZuzaProfilioRedagavimas extends ZuzaPrisijungimas {
 
 
-    public final static By profilioRedagavimoMygtukas = By.cssSelector(".registered-user__btn.btn");
-//    public final static By lytiesPasirinkimas = By.xpath("//*[@id=\"profile-update\"]/div[2]/div/div[2]/div/div");
+    public final static By profilioRedagavimoMygtukas = By.cssSelector("body > main > div.user__container.container > div.user__main > div > a");
+    static JavascriptExecutor jse = (JavascriptExecutor) driver;
+
 
     public ZuzaProfilioRedagavimas(WebDriver driver) {
 
@@ -21,22 +22,25 @@ public class ZuzaProfilioRedagavimas extends ZuzaPrisijungimas {
     }
 
     public static void profilioRedagavimas() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
 
         try {
 
             WebElement paspaustiMygtuka = driver.findElement(profilioRedagavimoMygtukas);
             paspaustiMygtuka.click();
-            Thread.sleep(50);
 
-            WebElement lytiesLokacija = driver.findElement(By.xpath("//*[@id=\"profile-update\"]/div[2]/div/div[1]/button"));
+
+            WebElement lytiesLokacija = driver.findElement(By.xpath("//*[@id=\"profile-update\"]/" +
+                    "div[2]/div/div[1]/button"));
             lytiesLokacija.click();
-            Thread.sleep(1000);
 
-            WebElement lytisVyras = driver.findElement(By.xpath("//*[@id=\"profile-update\"]/div[2]/div/div[2]/div/div/div[2]"));
+
+            WebElement lytisVyras = driver.findElement(By.xpath("//*[@id=\"profile-update\"]/div[2]/" +
+                    "div/div[2]/div/div/div[2]"));
             lytisVyras.click();
-            Thread.sleep(1000);
+
 
 //            Select lytiesPasirinkimai = new Select(driver.findElement(lytiesPasirinkimas));
 //            lytiesPasirinkimai.click();
@@ -49,18 +53,21 @@ public class ZuzaProfilioRedagavimas extends ZuzaPrisijungimas {
             // gimimos datos ivedimas - data susiveda, bet ties pavarde ir telefonu metamos klaidos
             WebElement gimimoData = driver.findElement(By.xpath("//input[@placeholder='YYYY-MM-DD']"));
             gimimoData.click();
-            gimimoData.sendKeys("20");
-            gimimoData.sendKeys(Keys.ARROW_LEFT);
-            Thread.sleep(1000);
-            gimimoData.sendKeys("04");
-            gimimoData.sendKeys(Keys.ARROW_LEFT);
-            Thread.sleep(1000);
-            gimimoData.sendKeys(Keys.ARROW_LEFT);
-            gimimoData.sendKeys("1985");
-            gimimoData.submit();
+            jse.executeScript("arguments[0].value='1985-04-20';", gimimoData);
+
+//            gimimoData.sendKeys("20");
+//            gimimoData.sendKeys(Keys.ARROW_LEFT);
+//            Thread.sleep(1000);
+//            gimimoData.sendKeys("04");
+//            gimimoData.sendKeys(Keys.ARROW_LEFT);
+//            Thread.sleep(1000);
+//            gimimoData.sendKeys(Keys.ARROW_LEFT);
+//            gimimoData.sendKeys("1985");
+//            gimimoData.submit();
 
             // vardo ivedimas
-            WebElement ivestiVarda = driver.findElement(By.xpath("//*[@id=\"profile-update\"]/div[4]/input"));
+            WebElement ivestiVarda = driver.findElement(By.xpath("//*[@id=\"profile-update\"]/div[4]/" +
+                    "input"));
             ivestiVarda.clear();
             ivestiVarda.sendKeys("Varlius");
             Thread.sleep(1000);
@@ -78,45 +85,53 @@ public class ZuzaProfilioRedagavimas extends ZuzaPrisijungimas {
             ivestiTelefona.sendKeys("+37065547965");
             Thread.sleep(2000);
             ivestiTelefona.submit();
-            ivestiTelefona.submit();
 
 
             // pakeitimu saugojimas
-            WebElement issaugotiPakeitimus = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("form[id='profile-update'] button[type='submit']")));
+            WebElement issaugotiPakeitimus = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"profile-update\"]/button")));
+            jse.executeScript("arguments[0].scrollIntoView();", issaugotiPakeitimus);
+            Thread.sleep(2000);
+            wait.until(ExpectedConditions.elementToBeClickable(issaugotiPakeitimus));
             issaugotiPakeitimus.click();
-
+            Thread.sleep(3000);
 
 
         } catch (Exception s) {
             System.out.println("Paskyros redagavimas negalimas: " + s.getMessage());
-        }
 
+
+        }
     }
 
-    public static void slaptazodzioKeitimas(String esamasSlaptazodis, String naujasSlaptazodis) throws InterruptedException {
+        public static void slaptazodzioKeitimas (String esamasSlaptazodis, String naujasSlaptazodis) throws
+        InterruptedException {
 
-        try {
+            try {
 
-            WebElement dabartinioSlaptazodzioIvedimas = driver.findElement(By.xpath("//*[@id=\"password-update\"]/div[1]/input"));
-            dabartinioSlaptazodzioIvedimas.sendKeys(esamasSlaptazodis);
+                WebElement dabartinioSlaptazodzioIvedimas = driver.findElement(By.xpath("//*[@id=\"password-update\"]/div[1]/input"));
+                dabartinioSlaptazodzioIvedimas.sendKeys(esamasSlaptazodis);
 
-            WebElement naujoSlaptazodzioIvedimas = driver.findElement(By.xpath("//*[@id=\"password-update\"]/div[2]/input"));
-            naujoSlaptazodzioIvedimas.sendKeys(naujasSlaptazodis);
+                WebElement naujoSlaptazodzioIvedimas = driver.findElement(By.xpath("//*[@id=\"password-update\"]/div[2]/input"));
+                naujoSlaptazodzioIvedimas.sendKeys(naujasSlaptazodis);
 
-            WebElement naujoSlaptazodzioIvedimas2 = driver.findElement(By.xpath("//*[@id=\"password-update\"]/div[3]/input"));
-            naujoSlaptazodzioIvedimas2.sendKeys(naujasSlaptazodis);
-
-            WebElement slaptazodzioPakeitimoIssaugojimas = driver.findElement(By.xpath("/html/body/main/div[2]/div[2]/div/div[3]/form/button"));
-            Thread.sleep(2000);
-            slaptazodzioPakeitimoIssaugojimas.click();
+                WebElement naujoSlaptazodzioIvedimas2 = driver.findElement(By.xpath("//*[@id=\"password-update\"]/div[3]/input"));
+                naujoSlaptazodzioIvedimas2.sendKeys(naujasSlaptazodis);
 
 
-        } catch (Exception r) {
-            System.out.println("Slaptazodzio keitimas nesekmingas: " + r.getMessage());
+                WebElement slaptazodzioPakeitimoIssaugojimas = driver.findElement(By.xpath("/html/body/main/div[2]/div[2]/div/div[3]/form/button"));
+                Thread.sleep(2000);
+                slaptazodzioPakeitimoIssaugojimas.click();
+
+
+            } catch (Exception r) {
+                System.out.println("Slaptazodzio keitimas nesekmingas: " + r.getMessage());
+            }
+
         }
-
     }
-}
+
+
+
 
 
 
